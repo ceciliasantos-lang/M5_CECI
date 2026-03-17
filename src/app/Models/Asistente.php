54cbe9d87dd74dda4e17111a20f2ccb270366ee2
ceciliasantos-lang <cecilia.santos@puebla.gob.mx>
2026-03-17 01:42:15 +0000

    se actualizan archivos para correr de forma correcta endpoints practica 1

--- src/app/Models/Asistente.php
+++ src/app/Models/Asistente.php
@@ -18,3 +18,4 @@ class Asistente extends Model
         'eventoid'
     ];
 }
+
--- src/database/migrations/2026_01_03_200504_create_eventos_table.php
+++ src/database/migrations/2026_01_03_200504_create_eventos_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration 
 {
-    public function up(): void
+    public function up()
     {
         Schema::create('eventos', function (Blueprint $table) {
             $table->id();
@@ -22,7 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('eventos');
     }
